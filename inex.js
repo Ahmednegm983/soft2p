ce86f65bbbc240745e66130a17efc624dd5a7b09
Your Name <you@example.com>
2023-05-05 19:28:46 +0200

    add env

--- inex.js
+++ inex.js
@@ -1,17 +1,19 @@
 import  express  from "express";
 import { engine } from 'express-handlebars';
-import mongoose from "mongoose";
-
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
+//const mongoConnectionUrl='';
+//const port=;
 mongoose.connect(process.env.mongoConnectionUrl);
+
 const app=express();
 
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', './templates');
+app.set('views', './templates'); 
 
 app.listen(process.env.port,()=>{
 
